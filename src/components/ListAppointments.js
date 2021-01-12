52e9c71051bb5235f1f4e15bca6fef1b58137cdd
Shabix <kmharouna@gmail.com>
2021-01-12 19:20:15 +0300

    refactored code for the list template

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,25 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class ListAppointments extends Component{
-    constructor(){
-        super();
-        this.state = {
-         myAppointments:[]
-        }
-      }
+class ListAppointments extends Component {
+  render() {
+    return (
+      <div className="appointment-list item-list mb-3">
+        {this.props.appointments.map(item => (
+          <div className="pet-item col media py-3">
+            <div className="mr-3">
+              <button className="pet-delete btn btn-sm btn-danger">X</button>
+            </div>
 
-    render(){
-        const listItems = this.props.appointments.map(item =>(
-            <>
-            <div>{item.petName}</div>
-            <div>{item.ownerName}</div>
-            <div>{item.aptNotes}</div>
-            <hr />
-      
-            </>
-          ));
-        return <div>{listItems}</div>
-    }
+            <div className="pet-info media-body">
+              <div className="pet-head d-flex">
+                <span className="pet-name">{item.petName}</span>
+                <span className="apt-date ml-auto">{item.aptDate}</span>
+              </div>
+
+              <div className="owner-name">
+                <span className="label-item">Owner: </span>
+                <span>{item.ownerName}</span>
+              </div>
+              <div className="apt-notes">{item.aptNotes}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default ListAppointments
+export default ListAppointments;
