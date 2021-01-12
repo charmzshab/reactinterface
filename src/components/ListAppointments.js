262ab9fb03fc9705e4e1412db69b57a5911d2954
Shabix <kmharouna@gmail.com>
2021-01-12 18:45:20 +0300

    passed json data through the props

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
 
 class ListAppointments extends Component{
+    constructor(){
+        super();
+        this.state = {
+         myAppointments:[]
+        }
+      }
+
     render(){
-        return <div>List--Appointments</div>
+        const listItems = this.props.appointments.map(item =>(
+            <>
+            <div>{item.petName}</div>
+            <div>{item.ownerName}</div>
+            <div>{item.aptNotes}</div>
+            <hr />
+      
+            </>
+          ));
+        return <div>{listItems}</div>
     }
 }
 
