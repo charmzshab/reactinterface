27f475c0b49b16fad6f98f0bc9c3ed916f89f64d
Shabix <kmharouna@gmail.com>
2021-01-19 12:09:10 +0300

    added logic for monitoring form state

--- src/components/AddAppointments.js
+++ src/components/AddAppointments.js
@@ -2,118 +2,153 @@ import React, { Component } from 'react';
 import {FaPlus} from 'react-icons/fa'
 
 class AddAppointments extends Component {
-  render() {
-    return (
-      <div
-        className={
-          'card textcenter mt-3 ' +
-          (this.props.formDisplay ? '' : 'add-appointment')
+
+    constructor(){
+        super();
+        this.state = {
+            petName: '',
+            ownerName: '',
+            aptDate: '',
+            aptTime: '',
+            aptNotes: '',
         }
-      >
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]:value
+        })
+
+    }
+
+    render() {
+    return (
+        <div
+        className={
+            'card textcenter mt-3 ' +
+            (this.props.formDisplay ? '' : 'add-appointment')
+        }
+        >
         <div className="apt-addheading card-header bg-primary text-white"
         onClick={this.props.toggleForm}>
-          <FaPlus /> Add Appointment
+            <FaPlus /> Add Appointment
         </div>
 
         <div className="card-body">
-          <form id="aptForm" noValidate>
+            <form id="aptForm" noValidate>
             <div className="form-group form-row">
-              <label
+                <label
                 className="col-md-2 col-form-label text-md-right"
                 htmlFor="petName"
                 readOnly
-              >
+                >
                 Pet Name
-              </label>
-              <div className="col-md-10">
+                </label>
+                <div className="col-md-10">
                 <input
-                  type="text"
-                  className="form-control"
-                  name="petName"
-                  placeholder="Pet's Name"
+                    type="text"
+                    className="form-control"
+                    name="petName"
+                    placeholder="Pet's Name"
+                    value={this.state.petName}
+                    onChange={this.handleChange}
                 />
-              </div>
+                </div>
             </div>
 
             <div className="form-group form-row">
-              <label
+                <label
                 className="col-md-2 col-form-label text-md-right"
                 htmlFor="ownerName"
-              >
+                >
                 Pet Owner
-              </label>
-              <div className="col-md-10">
+                </label>
+                <div className="col-md-10">
                 <input
-                  type="text"
-                  className="form-control"
-                  name="ownerName"
-                  placeholder="Owner's Name"
+                    type="text"
+                    className="form-control"
+                    name="ownerName"
+                    placeholder="Owner's Name"
+                    value={this.state.ownerName}
+                    onChange={this.handleChange}
                 />
-              </div>
+                </div>
             </div>
 
             <div className="form-group form-row">
-              <label
+                <label
                 className="col-md-2 col-form-label text-md-right"
                 htmlFor="aptDate"
-              >
+                >
                 Date
-              </label>
-              <div className="col-md-4">
+                </label>
+                <div className="col-md-4">
                 <input
-                  type="date"
-                  className="form-control"
-                  name="aptDate"
-                  id="aptDate"
+                    type="date"
+                    className="form-control"
+                    name="aptDate"
+                    id="aptDate"
+                    value={this.state.aptDate}
+                    onChange={this.handleChange}
                 />
-              </div>
-              <label
+                </div>
+                <label
                 className="col-md-2 col-form-label text-md-right"
                 htmlFor="aptTime"
-              >
+                >
                 Time
-              </label>
-              <div className="col-md-4">
+                </label>
+                <div className="col-md-4">
                 <input
-                  type="time"
-                  className="form-control"
-                  name="aptTime"
-                  id="aptTime"
+                    type="time"
+                    className="form-control"
+                    name="aptTime"
+                    id="aptTime"
+                    value={this.state.aptTime}
+                    onChange={this.handleChange}
                 />
-              </div>
+                </div>
             </div>
 
             <div className="form-group form-row">
-              <label className="col-md-2 text-md-right" htmlFor="aptNotes">
+                <label className="col-md-2 text-md-right" htmlFor="aptNotes">
                 Apt. Notes
-              </label>
-              <div className="col-md-10">
+                </label>
+                <div className="col-md-10">
                 <textarea
-                  className="form-control"
-                  rows="4"
-                  cols="50"
-                  name="aptNotes"
-                  id="aptNotes"
-                  placeholder="Appointment Notes"
+                    className="form-control"
+                    rows="4"
+                    cols="50"
+                    name="aptNotes"
+                    id="aptNotes"
+                    placeholder="Appointment Notes"
+                    value={this.state.aptNotes}
+                    onChange={this.handleChange}
                 />
-              </div>
+                </div>
             </div>
 
             <div className="form-group form-row mb-0">
-              <div className="offset-md-2 col-md-10">
+                <div className="offset-md-2 col-md-10">
                 <button
-                  type="submit"
-                  className="btn btn-primary d-block ml-auto"
+                    type="submit"
+                    className="btn btn-primary d-block ml-auto"
                 >
-                  Add Appointment
+                    Add Appointment
                 </button>
-              </div>
+                </div>
             </div>
-          </form>
+            </form>
         </div>
-      </div>
+        </div>
     );
-  }
+    }
 }
 
 export default AddAppointments;
